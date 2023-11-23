64119397e991f9dfe272e5bb1c2f8d3fd94e3b53
mhamzaaamir960 <mhamzaaamir960.gmail.com>
2023-11-23 22:29:26 +0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,109 @@
-# pricing_ui
-Designed Pricing UI page using Tailwind CSS
+This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+
+This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+
+## Learn More
+
+To learn more about Next.js, take a look at the following resources:
+
+- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
+- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+
+You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+
+## Deploy on Vercel
+
+The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+
+Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+
+
+<div className='flex justify-center items-center mx-10' >
+        <div className='bg-red-500 border-4 flex border-blue-500 absolute top-[14rem]
+        sm:w-[1000px] sm:h-[350px] w-[382px] h-[283px] '>
+
+            <div className=' bg-viol border-4 border-violet-200 rounded-l-xl max-h-[378px] max-w-[336]'>
+              <div className='m-10 text-center px-10 '>
+                <h1 className='text-xl font-bold mr-5'>Premium PRO</h1>
+                <span className='block text-5xl font-bold my-3'>$329</span>
+                <p className=' font-normal'>billed just once</p>
+                <button className=' font-light text-md border m-5 px-5 rounded-xl text-white bg-violet-500'>
+                  Get Started
+                </button>
+              </div >
+            </div>
+
+            <div className=' border-4 border-violet-200 rounded-r-xl max-h-[378px] max-w-[336]'>
+              <div className='m-10'>
+              <h1 className='font-light text-md ml-10'>Access these features when you get this pricing package for your business.</h1>
+              <ul className='p-6 text-start '>
+
+                <li className='flex place-items-center '>
+                  <Image src={'/tick.svg'} alt='logo' width={100} height={100} 
+                  className='h-[16px] w-[16px] mx-4 '/>
+                   International calling and messaging API
+                </li>
+
+                <li className='flex place-items-center my-2'>
+                  <Image src={'/tick.svg'} alt='logo' width={100} height={100} 
+                  className='h-[16px] w-[16px] mx-4 '/>
+                  Additional phone numbers
+                </li>
+
+                <li className='flex place-items-center '>
+                  <Image src={'/tick.svg'} alt='logo' width={100} height={100} 
+                  className='h-[16px] w-[16px] mx-4 '/>
+                  Automated messages via Zapier
+                </li>
+
+                <li className='flex place-items-center my-2'>
+                  <Image src={'/tick.svg'} alt='logo' width={100} height={100} 
+                  className='h-[16px] w-[16px] mx-4 '/>
+                  24/7 support and consulting
+                </li>     
+              </ul>
+            </div>
+          </div>
+
+        </div>
+
+          <div >
+            <ul className='flex justify-center flex-wrap'>
+              <li className='flex place-items-center w-52 font-semibold '>
+                <Image src={'/icon2.svg'} alt='icon' width={100} height={100} 
+                className='h-[40px] w-[40px] mr-4 '
+              />30 days money back Guarantee
+              </li>
+
+              <li className='flex place-items-center mx-16 w-52 font-semibold '>
+                <Image src={'/icon.svg'} alt='icon' width={100} height={100} 
+                className='h-[40px] w-[40px] mr-4 '
+              />30 days money back Guarantee
+              </li>
+
+              <li className='flex place-items-center w-52 font-semibold '>
+                <Image src={'/icon1.svg'} alt='icon' width={100} height={100} 
+                className='h-[40px] w-[40px] mr-4 '
+              />30 days money back Guarantee
+              </li>
+            </ul>
+          </div>
+        </div>
---
+++ app/page.tsx
@@ -0,0 +1,149 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+   <>
+      <div className='bg-white grid relative h-[100%] w-[100%] p-0 m-0'>
+
+
+
+        <div className='bg-violet-700 w-full h-96 absolute top-0 left-0 m-0'>
+          <div className='text-start sm:text-center mt-20 mx-4 sm:mt-28'>
+            <h1 className=' text-3xl sm:text-4xl lg:text-5xl font-bold text-white'>
+              Simple pricing for your business
+            </h1>
+            <p className='text-md text-xl mt-6 sm:text-xl lg:text-2xl text-white font-light'>
+              Plans that are carefully crafted to suit your business.
+            </p>
+          </div>
+       </div>
+
+
+       <div className='flex flex-wrap absolute justify-self-center top-[18rem] bg-white rounded-xl sm:w-[600px] 
+       sm:h-[350px] max-w-[1000px] lg:w-[900px] lg:h-[336px] w-[382px] h-[606px] mx-52 shadow-xl '>
+          <div className='sm:basis-1/3 bg-violet-100 sm:h-full lg:h-[336px] sm:rounded-l-xl flex 
+          justify-center items-center w-[382px] h-[283px] basis-100 rounded-t-xl'>
+           <div className='text-center '>
+            <h1 className='text-xl font-bold my-2'>Premium PRO</h1>
+                <span className='block text-5xl font-semibold '>$329</span>
+                <p className=' font-light text-lg'>billed just once</p>
+                <button className='border bg-violet-500 text-white sm:px-8 lg:px-16 
+                py-1 my-4 rounded-xl px-16'>
+                  Get Started
+                </button>
+          </div>
+        </div>
+
+        <div className=' sm:basis-2/3 rounded-r-xl sm:h-full flex justify-center items-center'>
+          <div className='p-10'>
+              <h1 className='text-gray-700 text-lg font-normal flex mb-4 '>Access these features when you get this pricing package for your business.</h1>
+              <ul className=''>
+                <li className='flex justify-start items-center my-4 '>
+                  <Image src={'/tick.svg'} alt='logo' width={100} height={100} 
+                  className='h-[16px] w-[16px] mr-4 '/>
+                   International calling and messaging API
+                </li>
+
+                <li className='flex justify-start items-center '>
+                  <Image src={'/tick.svg'} alt='logo' width={100} height={100} 
+                  className='h-[16px] w-[16px] mr-4 '/>
+                  Additional phone numbers
+                </li>
+
+                <li className='flex justify-start items-center my-4'>
+                  <Image src={'/tick.svg'} alt='logo' width={100} height={100} 
+                  className='h-[16px] w-[16px] mr-4  '/>
+                  Automated messages via Zapier
+                </li>
+
+                <li className='flex justify-start items-center'>
+                  <Image src={'/tick.svg'} alt='logo' width={100} height={100} 
+                  className='h-[16px] w-[16px] mr-4  '/>
+                  24/7 support and consulting
+                </li>     
+              </ul>
+            </div>
+          </div>
+         </div>
+
+          <div className='absolute flex top-[42rem] max-w-4xl justify-self-center items-center sm:mt-10 '>
+            <ul className='grid grid-cols-1 grid-rows-3 sm:grid-cols-2 sm:grid-rows-2 lg:grid-cols-3 lg:grid-rows-1
+                justify-center items-center gap-8 sm:gap-14 lg:gap-6 sm:w-[600px] lg:w-[800px] mt-64 mb-40 sm:mb-10 sm:mt-0'>
+              <li className='flex font-semibold '>
+                <Image src={'/icon2.svg'} alt='icon' width={100} height={100} 
+                className='h-[40px] sm:w-[40px] mr-3'
+              />
+              <p className='max-w-[300px] sm:w-[220px] mt-2 sm:m-0'>30 days money back Guarantee</p>
+              </li>
+
+              <li className='flex sm:mx-4 font-semibold '>
+                <Image src={'/icon.svg'} alt='icon' width={100} height={100} 
+                className='h-[40px] sm:w-[40px] mr-3 '
+              />
+              <p className='max-w-[300px] sm:w-[220px] mt-2 sm:m-0'>No setup fees 100% hassle-free</p>
+              </li>
+
+              <li className='flex font-semibold sm:justify-self-center lg:justify-normal sm:col-span-2 lg:col-span-1 '>
+                <Image src={'/icon1.svg'} alt='icon' width={100} height={100} 
+                className='h-[40px] sm:w-[40px] mr-3'
+              />
+              <p className='max-w-[280px] sm:w-[220px] mt-2 sm:m-0'>No monthly subscription Pay once and for all</p>
+              </li>
+            </ul>
+          </div>
+
+      </div>
+
+
+
+      {/* <div className='bg-slate-300 flex justify-center items-center h-screen'>
+        <div className='flex flex-wrap bg-white rounded-xl sm:w-[994px] sm:h-[336px] w-[382px] h-[606px] mx-10 '>
+          <div className='sm:basis-1/3 bg-violet-100 sm:h-full md:h-[336px] sm:rounded-l-xl flex 
+          justify-center items-center w-[382px] h-[283px] basis-100 rounded-t-xl'>
+           <div className='text-center '>
+            <h1 className='text-xl font-bold my-2'>Premium PRO</h1>
+                <span className='block text-5xl font-semibold '>$329</span>
+                <p className=' font-light text-lg'>billed just once</p>
+                <button className='border bg-violet-500 text-white sm:px-8 md:px-16 
+                py-1 my-4 rounded-xl px-16'>
+                  Get Started
+            </button>
+          </div>
+        </div>
+
+        <div className=' sm:basis-2/3 rounded-r-xl sm:h-full flex justify-center items-center'>
+          <div className='p-10'>
+              <h1 className='text-gray-700 text-lg font-normal flex mb-4 '>Access these features when you get this pricing package for your business.</h1>
+              <ul className=''>
+                <li className='flex justify-start items-center my-4 '>
+                  <Image src={'/tick.svg'} alt='logo' width={100} height={100} 
+                  className='h-[16px] w-[16px] mr-4 '/>
+                   International calling and messaging API
+                </li>
+
+                <li className='flex justify-start items-center '>
+                  <Image src={'/tick.svg'} alt='logo' width={100} height={100} 
+                  className='h-[16px] w-[16px] mr-4 '/>
+                  Additional phone numbers
+                </li>
+
+                <li className='flex justify-start items-center my-4'>
+                  <Image src={'/tick.svg'} alt='logo' width={100} height={100} 
+                  className='h-[16px] w-[16px] mr-4  '/>
+                  Automated messages via Zapier
+                </li>
+
+                <li className='flex justify-start items-center'>
+                  <Image src={'/tick.svg'} alt='logo' width={100} height={100} 
+                  className='h-[16px] w-[16px] mr-4  '/>
+                  24/7 support and consulting
+                </li>     
+              </ul>
+            </div>
+          </div>
+         </div>
+      </div> */}
+
+   </>
+  )
+}
